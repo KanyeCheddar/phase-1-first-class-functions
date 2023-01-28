2681c4641187a86d742c1e8ac060845f49dbc809
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-01-27 19:34:53 -0500

    this is an update with a second attempt for practice

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ function receivesAFunction(cb){
     return cb();
 } 
 
-function returnsANamedFunction(){
+/*function receivesAFunction(cb) {
+    return cb();
+}*/
+
+/*function returnsANamedFunction(){
     return function akeem() {
         return "dookie+3";
     }
-} 
+}*/
 
-function returnsAnAnonymousFunction() {
+function returnsANamedFunction(){
+    return function jamil() {
+        return 'trekky is great!';
+    }
+}
+
+/*function returnsAnAnonymousFunction() {
     return function () {
         return "akeem";
+    }
+}*/
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        return 'dookie'
     }
 }
